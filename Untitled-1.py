4ad799993c18362d3e0dc09fce1c2fd554388e76
Shraddha Pandey <pandeyshraddha805@gmail.com>
2025-08-12 10:48:29 +0530

    Initial commit

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+str = "my name is shraddha"
+x = str.capitalize()
+print(x)
+
+str2= "My last name is pandey"
+y = str2.center(40)
+print(y)
+
+str3 = "My college name is SRMCEM"
+z = str3.casefold()
+print(z)
